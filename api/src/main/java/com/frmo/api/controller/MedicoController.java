d8568f84b9c4ba99d95456b7edc0c5f8df21b67c
oliveirany <flavioranieli.dev@gmail.com>
2023-11-29 08:17:13 -0300

    finalizando primeiro curso da formacao - implementando endpoint de inativar medico no sistema

--- api/src/main/java/com/frmo/api/controller/MedicoController.java
+++ api/src/main/java/com/frmo/api/controller/MedicoController.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
@@ -26,9 +24,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository
-                .findAll(paginacao)
-                .map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @Transactional
@@ -36,5 +32,12 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
--- api/src/main/java/com/frmo/api/medico/Medico.java
+++ api/src/main/java/com/frmo/api/medico/Medico.java
@@ -29,6 +29,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
@@ -36,14 +38,22 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
 
     public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
-        this.nome = dados.nome() != null ? dados.nome() : this.nome;
-        this.telefone = dados.telefone() != null ? dados.telefone() : this.telefone;
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
         if (dados.endereco() != null) {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
     }
 
+    public void excluir() {
+        this.ativo = false;
+    }
 }
